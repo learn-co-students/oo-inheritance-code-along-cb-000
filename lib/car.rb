b1bcb7fe09f3a9588551aee87dcc5abc21deac2d
natasha kelly <kellynatashav@yahoo.com>
2017-04-25 21:49:38 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end # Overwriting go method from vehicle class and making a unique output from the car class.
+end
+# at the point at which the #go method is invoked, the compiler will first look in
+# the class to which the instance of car that we are calling the method on belongs.
+# If it finds a #go method there, it will execute that method.
+# If it doesn't find such a method there, it will move on to look in the
+# parent class that this class inherits from.
