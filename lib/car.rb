6dee9b09579cbdae18e75d1dcb032dad419897c2
Jen Poole <contact.vandepool@gmail.com>
2017-12-18 23:02:46 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
+# Car class will need access to the Vehicle class and will therefore need access to the file that contains that class
 require_relative "./vehicle.rb"
+
+# use the < to inherit the Car class from Vehicle class
+class Car < Vehicle
+
+# don't have to write a method for these because Car class inherits these methods from Vehicle class:
+  # initializes with two arguments, wheel size and number
+  # has a wheel_size
+  # has a wheel_number
+  # returns 'filling up!'
+end
